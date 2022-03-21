23120492b8253607c411d6317242a1b407c57a50
smit-1923 <smit.sce19@sot.pdpu.ac.in>
2022-03-21 13:32:52 +0530

    Complte Project

---
+++ src/components/App/App.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Header from "../Header/Header";
+import ResultContainer from "../ResultContainer/ResultContainer";
+import SearchBox from "../SearchBox/SearchBox";
+import './App.css'
+
+const name = require('@rstacruz/startup-name-generator');
+
+class App extends React.Component {
+    state = {
+        headerText: 'Name It!',
+        headerExpanded: true,
+        suggestedNames: [],
+    }
+
+    handleInputChange = (inputText) => {
+        this.setState({
+            headerExpanded: !inputText,
+            suggestedNames: inputText ? name(inputText) : [],
+        })
+    };
+
+    render() {
+        return (
+            <div>
+                <Header 
+                    headerExpanded={this.state.headerExpanded} 
+                    headerTitle={this.state.headerText}
+            />
+                <SearchBox onInputChange={this.handleInputChange}/>
+                <ResultContainer suggestedNames={this.state.suggestedNames} />
+            </div>
+        );
+    }
+}
+
+
+export default App;
---
+++ src/components/ResultContainer/ResultContainer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import NameCard from "../NameCard/NameCard";
+import './ResultContainer.css';
+
+const ResultContainer = ({suggestedNames}) => {
+
+    const suggestNameJsx = suggestedNames.map((suggestedName) => {
+        return <NameCard key={suggestedName} suggestedName={suggestedName}/>;
+    });
+
+    return (
+        <div className="results-container">
+            {suggestNameJsx}
+        </div>
+    );
+}
+
+export default ResultContainer; 
